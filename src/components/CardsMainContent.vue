55ded167936aaf94a6e35a9e6a6b2e52c6b61b28
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-28 16:18:01 +0100

    added main components and using v-for to print cards

---
+++ src/components/CardsMainContent.vue
@@ -0,0 +1,54 @@
+<template>
+    <main>
+        <div class="container">
+            <div v-for="(card, index) in cards" :key="index" class="card">
+                <img :src="card.thumb" alt="">
+                <div>{{card.series}}</div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'CardsMainCOntent',
+    props: {
+        cards: Array
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+
+main{
+    background-color: #333;
+
+    .container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin: 0 auto;
+        width: 1200px;
+
+        .card {
+            width: 200px;
+            margin: 20px 0;
+            color: #fff;
+            font-size: 13px;
+            text-transform: uppercase;
+
+            img {
+                width: 200px;
+                height: 200px;
+                padding: 10px;
+            }
+
+            > div {
+                padding: 0 10px;
+            }
+        }
+    }
+}
+</style>
